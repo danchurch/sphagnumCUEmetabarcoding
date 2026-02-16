05983daf6779f734fc02cf1969525cd4067d90b6
danchurch <danchurchthomas@gmail.com>
2026-02-16 11:23:59 +0100

    this looks like it will be a complicated merge

--- sequenceAnalysis/fastreeOnSinaRefSeqs.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-outDir="/home/daniel/Documents/projects/wernerSphagnum/sphagnumCUEmetabarcoding/sequenceAnalysis/findANME/"
-alignment="/home/daniel/Documents/projects/wernerSphagnum/sphagnumCUEmetabarcoding/sequenceAnalysis/findANME/refSeqsInSilva.afa"
-
-FastTree -gtr -nt < ${alignment} > ${alignment/afa/nwk}
-
-
--- sequenceAnalysis/runSinaOnRefSeqs.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-arbSilvaDB=/home/daniel/Documents/databases/silva/silva.arb
-outFolder=/home/daniel/Documents/projects/wernerSphagnum/sphagnumCUEmetabarcoding/sequenceAnalysis/findANME/
-sina  -i ps007_cleaned_sqrt_prop_RefSeqs.fa -r $arbSilvaDB -o $outFolder/refSeqsInSilva
-
