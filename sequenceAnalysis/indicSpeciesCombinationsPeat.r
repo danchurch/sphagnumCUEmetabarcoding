ed97e8850575e8986f45a3fe44cac62cbc21610c
daniel <danchurchthomas@gmail.com>
2026-07-22 09:12:09 +0200

    run indicSpec on opitplex

---
+++ sequenceAnalysis/indicSpeciesCombinationsPeat.r
@@ -0,0 +1,17 @@
+library(indicspecies)
+library(phyloseq)
+library(vegan)
+
+load("ps007_cleaned_sqrt_prop.rda")
+tax <- tax_table(ps007_cleaned_sqrt_prop)
+otutab <- as.data.frame(otu_table(ps007_cleaned_sqrt_prop))
+envData <- sample_data(ps007_cleaned_sqrt_prop)
+
+levels(envData$cluster)
+
+sc.list <- list()
+for (i in levels(envData$cluster)){
+  sc.i <- indicators(otutab, gr, group=i,
+                    control = how(nperm=999)) 
+}
+save(sc.list, file="indicSpeciesCombinationsPeat.rda")
